7b17a18ddbf9bfc094efa01e9341e90133b14659
hamletshu <ahngo13@naver.com>
2020-01-31 18:15:18 +0900

    feed-main, feed-main-section 메뉴 수정

--- public/js/common.js
+++ public/js/common.js
@@ -4,6 +4,23 @@ function logout(){
         location.reload();
     });
 }
+function goUserInfo(){
+    $.post('/user-info',{},function(resultData){
+        alert(resultData.message);
+    });
+}
+
+function goFriendList(){
+    $.post('/friend-list',{},function(resultData){
+        alert(resultData.message);
+    });
+}
+
+function goProfessWriteForm(){
+    $.post('/profess-write-form',{},function(resultData){
+        alert(resultData.message);
+    });
+}
 
 $(document).ready(function(){
     $(document).on("click","#goMain",function(){
